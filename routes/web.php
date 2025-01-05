5362950429a1b853796f695f10238f4dbc2a835d
felipessntos2 <santospinheiro627@gmail.com>
2025-01-04 23:25:08 -0300

    Update dashboad and confir rector

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
+Route::get('dashboard', \App\Livewire\Dashboard::class)
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
 
 require __DIR__.'/auth.php';
