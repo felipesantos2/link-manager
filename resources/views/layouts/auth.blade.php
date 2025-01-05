ff208130e88f1894cabecc0c86f16e21256e13b9
felipessntos2 <santospinheiro627@gmail.com>
2025-01-05 13:39:00 -0300

    update autj layout

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -11,8 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
- 
+        <!-- Scripts --> <!-- Styles -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-white antialiased">
         <section class="login-page">
